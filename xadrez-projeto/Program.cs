13633212daf42a92838dc24f6dd9d4810710a5f1
Leonardo Avelino <leoave46@gmail.com>
2021-03-17 21:32:47 -0300

    Começando a colocar as peças no tabuleiro.

--- xadrez-projeto/Program.cs
+++ xadrez-projeto/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace xadrez_projeto
 {
@@ -10,9 +11,15 @@ namespace xadrez_projeto
 
             Tabuleiro tab = new Tabuleiro(8, 8);
 
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+
             Tela.imprimirTabuleiro(tab);
 
             Console.ReadLine();
+
+
 
 
 
---
+++ xadrez-projeto/xadrez/Rei.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using tabuleiro;
+using xadrez;
+
+namespace xadrez_projeto.xadrez
+{
+    class Rei : Peca
+    {
+        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
+        { 
+        }
+            public override string ToString()
+        {
+
+            return "R";
+        }
+
+    }
+                
+}
