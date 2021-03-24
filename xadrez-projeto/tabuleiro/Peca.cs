e8a62315d8f40297e6c801442e42f8e088fd3051
Leonardo Avelino <leoave46@gmail.com>
2021-03-24 19:09:08 -0300

    Testes iniciais com movimentos possiveis das peças.

--- xadrez-projeto/tabuleiro/Peca.cs
+++ xadrez-projeto/tabuleiro/Peca.cs
@@ -2,7 +2,7 @@
 
 namespace tabuleiro
 {
-    class Peca
+    abstract class Peca
     {
 
         public Posicao posicao { get; set; }
@@ -24,6 +24,12 @@ namespace tabuleiro
         {
             qteMovimentos++;
         }
+
+
+        public abstract bool[,] movimentosPossiveis();
+      
+
+      
 
 
     }
